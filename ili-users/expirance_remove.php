7aa57972e6f6417883ce39b1fd6c13ff8067a398
Sakly Ayoub <saklyayoub@live.com>
2016-05-26 11:43:14 +0100

    fix notification drop expirance

--- ili-users/expirance_remove.php
+++ ili-users/expirance_remove.php
@@ -10,7 +10,17 @@ $company=$_GET['Company'];
 $idUser=$_GET['idUser'];
 $user=UserGetInfo($idUser);
 if($user==''){Redirect('index?message=14');}
-NotifAllWrite($idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$idUser.'">'.$user->FamilyName.' '.$user->FirstName.', suppression de l`expérience dans l`etablissement : '.$company);
-LogWrite("Suppression du l\'expérience : ".$company.", de l\'utilisateur : <a href=\"ili-users/user_profil?id=".$idUser."\">".$idUser."</a>");
-Refresh();
+else{
+	$idUserSession = $_SESSION['user_id'];
+	if($idUserSession==$idUser){
+		NotifAllWrite($idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$idUser.'">'.$user->FamilyName.' '.$user->FirstName.' a supprimé expérance dans l`etablissement : '.$company);
+	}
+	else{
+		$UserUpdated=UserGetInfo($idUser);
+		$UserUpdater=UserGetInfo($idUserSession);
+		NotifAllWrite($idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$idUser.'">'.$UserUpdater->FamilyName.' '.$UserUpdater->FirstName.' a supprimer l`experiance dans l`etablissement : '.$company.' de '.$UserUpdated->FamilyName.' '.$UserUpdated->FirstName);
+	}
+	LogWrite("Suppression du l\'expérience : ".$company.", de l\'utilisateur : <a href=\"ili-users/user_profil?id=".$idUser."\">".$idUser."</a>");
+	Refresh();
+}
 ?>
