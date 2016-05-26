791b75d091d728489643ddae0968b42d1fbdabbb
Sakly Ayoub <saklyayoub@live.com>
2016-05-26 15:42:32 +0100

    Fix Notification ban/deban

--- ili-users/user_ban.php
+++ ili-users/user_ban.php
@@ -8,8 +8,13 @@ Authorization('2');
 $idUser=$_GET['id'];
 $user=UserGetInfo($idUser);
 if($user==''){Redirect('index?message=14');}
+else{
 UserBan($idUser);
-NotifAllWrite($user->idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$user->idUser.'">'.$user->FamilyName.' '.$user->FirstName.', a été banni');
-LogWrite("Utilisateur : <a href=\"ili-users/user_profil?id=".$user->idUser."\">".$user->idUser."</a> a été <strong>banni</strong>");
-Refresh();
+	$idUserSession = $_SESSION['user_id'];
+	$UserUpdated=UserGetInfo($idUser);
+	$UserUpdater=UserGetInfo($idUserSession);
+	NotifAllWrite($idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$idUser.'">'.$UserUpdater->FamilyName.' '.$UserUpdater->FirstName.' a banni '.$UserUpdated->FamilyName.' '.$UserUpdated->FirstName);
+	LogWrite("Utilisateur : <a href=\"ili-users/user_profil?id=".$user->idUser."\">".$user->idUser."</a> a été <strong>banni</strong>");
+	Refresh();
+	}
 ?>
--- ili-users/user_deban.php
+++ ili-users/user_deban.php
@@ -8,8 +8,13 @@ Authorization('2');
 $idUser=$_GET['id'];
 $user=UserGetInfo($idUser);
 if($user==''){Redirect('index?message=14');}
-UserDeban($idUser);
-NotifAllWrite('', '', '<a href="'.$URL.'ili-users/user_profil?id='.$user->idUser.'">'.$user->FamilyName.' '.$user->FirstName.', a été débanni');
-LogWrite("Utilisateur : <a href=\"ili-users/user_profil?id=".$user->idUser."\">".$user->idUser."</a> a été <strong>débanni</strong>");
-Refresh();
+else{
+	UserDeban($idUser);
+	$idUserSession = $_SESSION['user_id'];
+	$UserUpdated=UserGetInfo($idUser);
+	$UserUpdater=UserGetInfo($idUserSession);
+	NotifAllWrite($idUser, '', '<a href="'.$URL.'ili-users/user_profil?id='.$idUser.'">'.$UserUpdater->FamilyName.' '.$UserUpdater->FirstName.' a banni '.$UserUpdated->FamilyName.' '.$UserUpdated->FirstName);
+	LogWrite("Utilisateur : <a href=\"ili-users/user_profil?id=".$user->idUser."\">".$user->idUser."</a> a été <strong>débanni</strong>");
+	Refresh();
+}
 ?>
